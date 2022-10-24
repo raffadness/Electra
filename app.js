476c13aaf1950fbd3d6f103002797a12bdec5b4c
Raff Adness <glorious332@gmail.com>
2022-10-24 08:34:43 +0700

    Puwush

---
+++ app.js
@@ -0,0 +1,81 @@
+// GALLERY SLIDE
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+// EMAIL JS
+
+const validate = () => {
+  let name = document.querySelector('#nama')
+  let email = document.querySelector('#email')
+  let message = document.querySelector('#pesan')
+  let submit = document.querySelector('#submit')
+
+  submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if ( name.value == '' || email.value == '' || message.value == '' ) {
+      InputEmpty();
+    } else {
+      sendMail(name.value, email.value, message.value);
+      success();
+    }
+  })
+}
+
+const sendMail = (name, email, message) => {
+  emailjs.send('service_l5hk58n', 'template_xbpy3dp',
+  {
+    from_name : name,
+    to_name : email,
+    message : message
+  })
+}
+
+const success = () => {
+  swal({
+    title : 'Good job!',
+    text : 'Successfully sent message !',
+    icon : 'success',
+    button : 'OK!'
+  })
+}
+
+const error = () => {
+  swal({
+    title: 'Oops..!',
+    text: 'Something went wrong, message could not be sent!',
+    icon: 'error',
+    button: 'OK!'
+  })
+}
+
+const InputEmpty = () => {
+  swal({
+    title: 'Oops..!',
+    text: 'Input fields are required!',
+    icon: 'error',
+    button: 'OK!'
+  })
+}
+
+( function () {
+  emailjs.init('tHMIhzeblzCs7EOVX');
+} )()
+
+validate()
