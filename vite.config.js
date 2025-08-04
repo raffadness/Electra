30dafdc35d4cc2598f2a2d567233303e53c976b2
Raff Adness <glorious332@gmail.com>
2025-08-04 12:34:44 +0700

    Fix Netlify MIME type issues

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    headers: {
+      "Content-Type": "application/javascript",
+    },
+  },
 });
